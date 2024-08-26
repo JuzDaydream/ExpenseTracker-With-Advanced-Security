cfd115eb5053f0185ff075201c36267fd3f97044
juzdaydream <juzdaydream@gmail.com>
2024-08-26 22:35:32 +0800

    retrofit

--- app/src/main/java/com/example/expensetracker/network/RetrofitClient.kt
+++ app/src/main/java/com/example/expensetracker/network/RetrofitClient.kt
@@ -1,20 +1,34 @@
 package com.example.expensetracker.network
 
 
+
+
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 object RetrofitClient {
-   private const val BASE_URL = "https://api.meaningcloud.com/"
+    private const val BASE_URL = "https://api.meaningcloud.com/"
+
 
     private val gson = GsonBuilder()
         .setLenient()
         .create()
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+
+
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)  // Attach the OkHttpClient
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
     val service: MeaningCloudService = retrofit.create(MeaningCloudService::class.java)
